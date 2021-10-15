74fe406d3bd7da3d9ac592338a71880d8e2a49de
zyt564 <2408867874@qq.com>
2021-10-15 21:11:07 +0800

    Initial commit

---
+++ src/spring/helloworld.java
@@ -0,0 +1,13 @@
+package spring;
+
+public class helloworld {
+    String name;
+    public void setName(String name){
+          this.name=name;
+
+    }
+    public void sayHello(){
+        System.out.println("Hello"+name);
+    }
+
+}
---
+++ src/spring/main.java
@@ -0,0 +1,10 @@
+package spring;
+
+public class main {
+    public static void main(String[] args){
+        helloworld helloworld =new helloworld();
+        helloworld.setName("world");
+        helloworld.sayHello();
+    }
+
+}
